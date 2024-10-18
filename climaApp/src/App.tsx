ceb9f5a509359b95efce0d60a40ef8db0838171a
zuoki <jgcorrea56@gmail.com>
2024-10-18 18:13:53 -0300

    colores3

--- climaApp/src/App.tsx
+++ climaApp/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <div id='app'>
         <Header/>
          <h1 className='text-2xl w-56 items-center flex justify-center mx-auto p-2 mt-10 bg-slate-600 rounded-md text-white'>Clima App ☀️</h1>
+         <h3>agregar cards</h3>
       </div>
     
   )
