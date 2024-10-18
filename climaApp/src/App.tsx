68eaaac2d83a9151e7fc6d1e2719a5e0b9bf9fb6
zuoki <jgcorrea56@gmail.com>
2024-10-18 18:14:27 -0300

    cards

--- climaApp/src/App.tsx
+++ climaApp/src/App.tsx
@@ -9,6 +9,8 @@ function App() {
         <Header/>
          <h1 className='text-2xl w-56 items-center flex justify-center mx-auto p-2 mt-10 bg-slate-600 rounded-md text-white'>Clima App ☀️</h1>
          <h3>agregar cards</h3>
+         <h3>agregar cards</h3>
+         <h3>agregar cards</h3>
       </div>
     
   )
